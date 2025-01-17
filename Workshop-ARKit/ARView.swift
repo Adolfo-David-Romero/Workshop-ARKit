f21b0e1c2bb47dd25c2a7e61d0bdff7c60e038a7
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-01-17 02:43:52 -0500

    Changed Target OS. Added discoball entity. It's a little too big so i'll have to resize it.

--- Workshop-ARKit/ARView.swift
+++ Workshop-ARKit/ARView.swift
@@ -11,26 +11,32 @@ import RealityKit
 struct ARView : View {
 
     var body: some View {
-        RealityView { content in
-
-            // Create a cube model
-            let model = Entity()
-            let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-            let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-            model.components.set(ModelComponent(mesh: mesh, materials: [material]))
-            model.position = [0, 0.05, 0]
-
-            // Create horizontal plane anchor for the content
-            let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-            anchor.addChild(model)
-
-            // Add the horizontal plane anchor to the scene
-            content.add(anchor)
-
-            content.camera = .spatialTracking
-
+        if #available(iOS 18.0, *) {
+            RealityView { content in
+                
+                //Load the .usdz model asynchronously
+                if let discoBall = try? await ModelEntity(named: "Disco_ball_with_colored_lights") {
+                    content.add(discoBall)
+                    
+                    discoBall.position = [0, 0.05, 0] // Set the position of the disco ball
+                    
+                    // Create horizontal plane anchor for the content
+                    let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
+                    anchor.addChild(discoBall)
+                    
+                    // Add the horizontal plane anchor to the scene
+                    content.add(anchor)
+                    
+                    content.camera = .spatialTracking
+                }
+                
+                
+                
+            }
+            .edgesIgnoringSafeArea(.all)
+        } else {
+            // Fallback on earlier versions
         }
-        .edgesIgnoringSafeArea(.all)
     }
 
 }
